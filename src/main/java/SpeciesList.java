7017dd3f5d0a7c230c299e17c968aa055c63d41d
rr <nadavv005@gmail.com>
2020-08-09 14:19:03 +0300

    speciesList arrangement

--- src/main/java/SpeciesList.java
+++ src/main/java/SpeciesList.java
@@ -3,20 +3,19 @@ import java.util.*;
 public class SpeciesList {
     private static final int DEFAULT_LEVEL =5;
     private static final TreeMap<Integer, Species> list = new TreeMap<>();
-    public static MoveList mList = new MoveList();
     private static Random rnd = new Random();
 
     static {
         Species pkmn;
-        pkmn = new Species(1, "Bulbasaur", new BaseStats(49, 65, 49, 65, 65, 45), 64, 12, 4, new Move[]{MoveList.getMove(22), MoveList.getMove(33), MoveList.getMove(51)});
+        pkmn = new Species(1, "Bulbasaur", new BaseStats(49, 65, 49, 65, 65, 45), 64,new int[]{0,0,0,1,0,0}, 12, 4, new int[]{1,45,1,33,3,22,6,74,9,73,12,75,15,77,15,79,18,402,21,36,24,230,27,235,30,388,33,38,36,76});
         list.put(pkmn.id(), pkmn);
-        pkmn = new Species(4, "Charmander", new BaseStats(39, 52, 43, 60, 50, 65), 62, 10, 0, new Move[]{MoveList.getMove(52), MoveList.getMove(33), MoveList.getMove(51)});
+        pkmn = new Species(4, "Charmander", new BaseStats(39, 52, 43, 60, 50, 65), 62,new int[]{0,0,0,0,0,1}, 10, 0, new Move[]{MoveList.getMove(52), MoveList.getMove(33), MoveList.getMove(51)});
         list.put(pkmn.id(), pkmn);
-        pkmn = new Species(7, "Squirtle", new BaseStats(44, 48, 65, 50, 64, 43), 63, 11, 0, new Move[]{MoveList.getMove(55), MoveList.getMove(33)});
+        pkmn = new Species(7, "Squirtle", new BaseStats(44, 48, 65, 50, 64, 43), 63,new int[]{0,0,1,0,0,0}, 11, 0, new Move[]{MoveList.getMove(55), MoveList.getMove(33)});
         list.put(pkmn.id(), pkmn);
-        pkmn = new Species(39, "Jigglypuff",new BaseStats(115,45,20,45,25,20),95, 1, 18, new Move[]{MoveList.getMove(3), MoveList.getMove(1)});
+        pkmn = new Species(39, "Jigglypuff",new BaseStats(115,45,20,45,25,20),95,new int[]{2,0,0,0,0,0}, 1, 18, new Move[]{MoveList.getMove(3), MoveList.getMove(1)});
         list.put(pkmn.id(), pkmn);
-        pkmn = new Species(66, "Machop", new BaseStats(70,80,50,35,35,35),61, 2, 0, new Move[]{MoveList.getMove(2), MoveList.getMove(24)});
+        pkmn = new Species(66, "Machop", new BaseStats(70,80,50,35,35,35),61,new int[]{0,1,0,0,0,0}, 2, 0, new Move[]{MoveList.getMove(2), MoveList.getMove(24)});
         list.put(pkmn.id(), pkmn);
         insertDBSpecies();
     }
